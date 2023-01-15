27bb804968021f2aee08afcfb3e4433ab694b2f2
McChronicle <mcchronicle@outlook.com>
2023-01-15 02:51:08 +0100

    added chromium version

---
+++ chromium/content.js
@@ -0,0 +1,12 @@
+
+
+const onMutation = () => {
+    const element = document.querySelector('.watch-video--skip-content-button')
+    if (element) {
+      element.click()
+    }
+  }
+  
+  const observer = new MutationObserver(onMutation)
+  
+  observer.observe(document.body, {childList: true, subtree: true})
