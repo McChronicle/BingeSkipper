8b655a649c2ee8706b93c8a158f9654639dccd97
McChronicle <mcchronicle@outlook.com>
2023-05-05 17:45:43 +0200

    implemented config for chromium version

--- chromium/content.js
+++ chromium/content.js
@@ -7,12 +7,40 @@
  */
 
 const onMutation = () => {
-  const element = document.querySelector('.watch-video--skip-content-button, [data-uia="next-episode-seamless-button"], [data-uia="next-episode-seamless-button-draining"], [data-uia="interrupt-autoplay-continue"]');
-  if (element) {
-    element.click();
-  }
+    //const element = document.querySelector('.watch-video--skip-content-button, [data-uia="next-episode-seamless-button"], [data-uia="next-episode-seamless-button-draining"], [data-uia="interrupt-autoplay-continue"]');
+    
+    const skipIntroOrRecapButton = document.querySelector('.watch-video--skip-content-button');
+    const nextEpisodeButton = document.querySelector('[data-uia="next-episode-seamless-button"], [data-uia="next-episode-seamless-button-draining"]');
+    const continueWatchingButton = document.querySelector('[data-uia="interrupt-autoplay-continue"]');
+    
+    if (skipIntroOrRecapButton) {
+        // get config from storage
+        chrome.storage.sync.get(["skipIntroAndRecap"], function(result) {
+            // click the button if the config is true
+            if (result.skipIntroAndRecap) {
+                skipIntroOrRecapButton.click();
+            }
+        });
+    } else if (nextEpisodeButton) {
+        // get config from storage
+        chrome.storage.sync.get(["autoClickNextEpisode"], function(result) {
+            // click the button if the config is true
+            if (result.autoClickNextEpisode) {
+                nextEpisodeButton.click();
+            }
+        });
+    } else if (continueWatchingButton) {
+        // get config from storage
+        chrome.storage.sync.get(["autoClickContinueWatching"], function(result) {
+            // click the button if the config is true
+            if (result.autoClickContinueWatching) {
+                continueWatchingButton.click();
+            }
+        });
+    }
+
 }
-  
+    
 const observer = new MutationObserver(onMutation);
-  
+    
 observer.observe(document.body, {childList: true, subtree: true});
