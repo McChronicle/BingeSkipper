6b767b7f2a9e58ef233a5775de0f7cdcaf3446ae
McChronicle <mcchronicle@mcchronicle.de>
2023-05-07 13:32:26 +0200

    set the popup based on the used streaming service

---
+++ chromium/service_worker.js
@@ -0,0 +1,30 @@
+
+
+function setPopup(tab) {
+    // check the url of the active tab and set the popup accordingly
+    if (tab.url.includes("netflix.com")) {
+        chrome.action.setPopup({popup: "netflix/popup.html"});
+    } else if (tab.url.includes("amazon.")) {
+        chrome.action.setPopup({popup: "amazon/popup.html"});
+    } else {
+        chrome.action.setPopup({popup: "default/popup.html"});
+    }
+}
+
+
+// register a tab listener to determine the active tab
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    // get the active tab and set the popup
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        setPopup(tab);
+    });
+});
+
+// register a url listener to determine the current url
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // set the popup if the url has changed
+    if (changeInfo.url) {
+        setPopup(tab);
+    }
+});
+
