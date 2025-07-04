95e67e7d1f65bcb382623b39dcc374baa33e39cd
McChronicle <mcchronicle@outlook.com>
2025-07-04 16:27:19 +0200

    Refactored configuration storage and handling

--- src/content.ts
+++ src/content.ts
@@ -6,29 +6,22 @@
  * [data-uia="interrupt-autoplay-continue"] is the attribute for the "Continue Watching" button
  */
 
-import { Configuration, loadConfig } from "./storage";
-
+import { config } from "./storage";
 
 const onMutation: () => Promise<void> = async () => {
-
-    const skipIntroOrRecapButton: HTMLElement | null = document.querySelector(".watch-video--skip-content-button");
-    const nextEpisodeButton: HTMLElement | null = document.querySelector("[data-uia='next-episode-seamless-button'], [data-uia='next-episode-seamless-button-draining']");
-    const continueWatchingButton: HTMLElement | null = document.querySelector("[data-uia='interrupt-autoplay-continue']");
-
-    const config: Configuration = await loadConfig();
-
-    if (skipIntroOrRecapButton && config.skipIntroAndRecap) {
-        skipIntroOrRecapButton.click();
-    } else if (nextEpisodeButton && config.autoClickNextEpisode) {
-        nextEpisodeButton.click();
-    } else if (continueWatchingButton && config.autoClickContinueWatching) {
-        continueWatchingButton.click();
+    if (config.skipIntroAndRecap) {
+        const skipIntroOrRecapButton: HTMLElement | null = document.querySelector(".watch-video--skip-content-button");
+        skipIntroOrRecapButton?.click();
     }
-
+    if (config.autoClickNextEpisode) {
+        const nextEpisodeButton: HTMLElement | null = document.querySelector("[data-uia='next-episode-seamless-button'], [data-uia='next-episode-seamless-button-draining']");
+        nextEpisodeButton?.click();
+    }
+    if (config.autoClickContinueWatching) {
+        const continueWatchingButton: HTMLElement | null = document.querySelector("[data-uia='interrupt-autoplay-continue']");
+        continueWatchingButton?.click();
+    }
 }
-
 
 const observer: MutationObserver = new MutationObserver(onMutation);
 observer.observe(document.body, { childList: true, subtree: true });
-
-
--- src/storage.ts
+++ src/storage.ts
@@ -13,7 +13,22 @@ export const DEFAULT_CONFIG: Configuration = {
 
 export const syncStorage: chrome.storage.SyncStorageArea = chrome.storage.sync;
 
+export const config: Configuration = await loadConfig();
+
 export async function loadConfig(): Promise<Configuration> {
     const res: Configuration = await syncStorage.get(["autoClickContinueWatching", "autoClickNextEpisode", "skipIntroAndRecap"]);
     return res;
 };
+
+type ConfigValue = "autoClickContinueWatching" | "autoClickNextEpisode" | "skipIntroAndRecap";
+type MaybeConfigValue = ConfigValue | null;
+
+syncStorage.onChanged.addListener((changes) => {
+    for (const key in changes) {
+        const keyConfigValue: MaybeConfigValue = key as MaybeConfigValue;
+        if (keyConfigValue !== null) {
+            const change: chrome.storage.StorageChange = changes[keyConfigValue];
+            config[keyConfigValue] = change.newValue;
+        }
+    }
+});
