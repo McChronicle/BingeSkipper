749403480fd0494d29a89542008efa420f6256ed
McChronicle <mcchronicle@outlook.com>
2025-07-05 15:07:24 +0200

    Updated popup to use config cache

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,19 +1,18 @@
-import { loadConfig } from "../storage";
+import { config } from "../storage";
 
 // load checkbox state from synced storage when popup is opened
-loadConfig().then((config) => {
-    const skipIntroCheckbox: HTMLInputElement = document.getElementById("skip-intro") as HTMLInputElement;
-    skipIntroCheckbox.checked = config.skipIntro;
+const skipIntroCheckbox: HTMLInputElement = document.getElementById("skip-intro") as HTMLInputElement;
+skipIntroCheckbox.checked = config.skipIntro;
 
-    const skipRecapCheckbox: HTMLInputElement = document.getElementById("skip-recap") as HTMLInputElement;
-    skipRecapCheckbox.checked = config.skipRecap;
+const skipRecapCheckbox: HTMLInputElement = document.getElementById("skip-recap") as HTMLInputElement;
+skipRecapCheckbox.checked = config.skipRecap;
 
-    const autoClickNextEpisodeCheckbox: HTMLInputElement = document.getElementById("auto-click-next-episode") as HTMLInputElement;
-    autoClickNextEpisodeCheckbox.checked = config.autoClickNextEpisode;
+const autoClickNextEpisodeCheckbox: HTMLInputElement = document.getElementById("auto-click-next-episode") as HTMLInputElement;
+autoClickNextEpisodeCheckbox.checked = config.autoClickNextEpisode;
 
-    const autoClickContinueWatchingCheckbox: HTMLInputElement = document.getElementById("auto-click-continue-watching") as HTMLInputElement;
-    autoClickContinueWatchingCheckbox.checked = config.autoClickContinueWatching;
-});
+const autoClickContinueWatchingCheckbox: HTMLInputElement = document.getElementById("auto-click-continue-watching") as HTMLInputElement;
+autoClickContinueWatchingCheckbox.checked = config.autoClickContinueWatching;
+
   
 // save checkbox state to synced storage when user checks or unchecks a checkbox
 document.querySelectorAll("input[type=checkbox]").forEach(function(checkbox: Element) {
